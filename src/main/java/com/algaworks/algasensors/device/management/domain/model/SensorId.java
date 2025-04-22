bc1219ee96f204d61fbace71755922348837db0b
Antonio Pires <tonan@outlook.com.br>
2025-04-22 08:12:08 -0300

    10.14. Implementing the temperature log query

--- src/main/java/com/algaworks/algasensors/device/management/domain/model/SensorId.java
+++ src/main/java/com/algaworks/algasensors/device/management/domain/model/SensorId.java
@@ -3,16 +3,22 @@ package com.algaworks.algasensors.device.management.domain.model;
 import io.hypersistence.tsid.TSID;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class SensorId implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7123382566342295763L;
 
     private TSID value;
 
